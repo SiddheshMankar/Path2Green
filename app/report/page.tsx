62db44001a238465aaadad6fe2abe3d93dc267b7
siddhesh mankar <siddheshmankar70@gmail.com>
2024-10-23 17:53:47 +0530

    comm

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState, useCallback, useEffect } from 'react'
 import {  MapPin, Upload, CheckCircle, Loader } from 'lucide-react'
 import { Button } from '@/components/ui/button'
