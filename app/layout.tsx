6ef89c38f0b84c2be989cc5cfe2564acbac91db3
siddhesh mankar <siddheshmankar70@gmail.com>
2024-10-23 17:19:04 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   const [sidebarOpen, setSidebarOpen] = useState(false)
-  const [totalEarnings, setTotalEarnings] = useState(0)
+  const [totalEarnings, setTotalEarnings] = useState<{ id: number; name: string; cost: number; description: string | null; collectionInfo: string; }[]>([])
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
